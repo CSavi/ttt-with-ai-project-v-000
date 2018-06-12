8980a50b3f038a7820d526c8578a0902327e0fd3
Carrie Savinell <carrielane89@gmail.com>
2018-06-12 13:25:25 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -14,7 +14,7 @@ WIN_COMBINATIONS = [
 ]
 
 #<<<<<<< HEAD
-  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+  #def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
 #=======
   def initialize(player_1 = "X", player_2 = "O", board= Board.new)
 #>>>>>>> 7bafa383bdf5f15e5e3ffe5140aa0bb139b36067
